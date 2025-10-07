07c9d7b554ecaca936897d26f63277dced67ec27
diego PC escritorio <dyeqos@gmail.com>
2025-10-06 20:25:54 -0400

    Paso 1

--- java/step1_oop.java
+++ java/step1_oop.java
@@ -4,15 +4,24 @@
  * comportamiento de las cuentas y de la operación de transferencia,
  * facilitando así la mantenibilidad y la extensibilidad.
  */
+
+/**
+ * se instancia la clase CuentaBancaria
+ */
 class CuentaBancaria {
+    //encapsulamiento de variables 
+    //solo se pueden modificar con getters y setters
+    //en este caso los métodos modifican los variables de la clase
     private String titular;
     private double saldo;
 
+    //construye la clase CuentaBancaria recibiendo el titular y saldo inicial de la cuenta
     public CuentaBancaria(String titular, double saldoInicial) {
         this.titular = titular;
         this.saldo = saldoInicial;
     }
 
+    // valida si tiene saldo y es superior al monto
     public boolean retirar(double monto) {
         if (monto <= saldo) {
             saldo -= monto;
@@ -21,20 +30,26 @@ class CuentaBancaria {
         return false;
     }
 
+    //deposita, sumando el monto enviado al saldo
     public void depositar(double monto) {
         saldo += monto;
     }
 
+    //retorna el saldo de una cuenta
     public double getSaldo() {
         return saldo;
     }
 
+    //retorna el nombre del titular de la cuenta
     public String getTitular() {
         return titular;
     }
 }
 
 class Transferencia {
+    /**
+     * Método 'realizar' recibe dos cuentas origin destino y un monto
+     */
     public void realizar(CuentaBancaria origen, CuentaBancaria destino, double monto) {
         if (origen.retirar(monto)) {
             destino.depositar(monto);
@@ -47,9 +62,10 @@ class Transferencia {
 
 public class Step1OOP {
     public static void main(String[] args) {
+        //se crean dos cuentas bancarias
         CuentaBancaria cuentaA = new CuentaBancaria("A", 1000);
         CuentaBancaria cuentaB = new CuentaBancaria("B", 500);
-        Transferencia transferencia = new Transferencia();
+        Transferencia transferencia = new Transferencia();//instancia la clase transferencia
         transferencia.realizar(cuentaA, cuentaB, 200);
         System.out.println("Saldo A: " + cuentaA.getSaldo() + ", Saldo B: " + cuentaB.getSaldo());
     }
