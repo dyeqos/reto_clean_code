915fa162e19921baa69bb007cbd22533fe4c82ca
diego PC escritorio <dyeqos@gmail.com>
2025-10-06 21:22:19 -0400

    Paso 2

--- java/step2_discount.java
+++ java/step2_discount.java
@@ -2,14 +2,21 @@
  * Clase utilitaria para calcular descuentos según el tipo de cliente.
  */
 public class DiscountCalculator {
+    //El TDD (Test-Driven Development) o Desarrollo Guiado por Pruebas es una metodología 
+    //de programación donde escribes primero las pruebas (tests) antes del código funcional,
+    // y es muy útil cuando quieres refactorizar código sin romper lo que ya funciona.
+
+    //Listado de descuentos
+    private static final Map<String, Double> descuentosMap = Map.of(
+        "preferente", 0.9,
+        "vip", 0.8
+        //agregar mas descuentos
+    );
+
+
     public static double calcularDescuento(double precio, String tipoCliente) {
-        switch (tipoCliente) {
-            case "preferente":
-                return precio * 0.9;
-            case "vip":
-                return precio * 0.8;
-            default:
-                return precio;
-        }
+        // Si el tipo no existe, se aplica 1.0 (sin descuento)
+        double factor = descuentosMap.getOrDefault(tipoCliente, 1.0);
+        return precio * factor;
     }
 }
