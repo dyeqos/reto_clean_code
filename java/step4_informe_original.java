55391aae8c2c6ba6c597c74e0b43af66f11a43e3
diego PC escritorio <dyeqos@gmail.com>
2025-10-06 21:51:54 -0400

    Paso 4

--- java/step4_informe_original.java
+++ java/step4_informe_original.java
@@ -4,16 +4,57 @@
  * sola clase.  Utiliza este archivo como base para refactorizar en el
  * ejercicio del paso 4.
  */
-public class Informe {
-    public void generarPDF() {
+
+interface GeneradorPDF {
+    void generarPDF(InformeData data);
+}
+
+public class InformePDF implements GeneradorPDF {
+    @Override
+    public void generarPDF(InformeData data) {
         // Generar un documento PDF
     }
+}
 
-    public void enviarEmail() {
+interface NotificadorEmail {
+    void enviarEmail(InformeData data);
+}
+public class EmailNotificador implements NotificadorEmail {
+    @Override
+    public void enviarEmail(InformeData data) {
         // Enviar el informe por correo electrónico
     }
+}
 
-    public void guardarEnBaseDatos() {
+interface RepositorioDB {
+    void guardarEnBaseDatos(InformeData data);
+}
+
+class InformeRepositorio implements RepositorioDB {
+    @Override
+    public void guardarEnBaseDatos(InformeData data) {
         // Guardar el informe en la base de datos
     }
+}
+
+class InformeService {
+    private GeneradorPDF generador;
+    private NotificadorEmail notificador;
+    private RepositorioDB repositorio;
+
+    public InformeService(GeneradorPDF generador, NotificadorEmail notificador, RepositorioDB repositorio) {
+        this.generador = generador;
+        this.notificador = notificador;
+        this.repositorio = repositorio;
+    }
+
+    public void procesar(InformeData data) {
+        generador.generarPDF(data);
+        notificador.enviarEmail(data);
+        repositorio.guardarEnBaseDatos(data);
+    }
+}
+
+class InformeData {
+    // Campos del informe (placeholder)
 }
